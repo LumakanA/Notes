a6dc4e1eef91c31e89ee2e950f55d72dcc3bc986
LumakanA <lumakan@yandex.com>
2023-02-03 11:14:22 +0300

    КТ2

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notes.databinding.RegistrationBinding
+import com.example.notes.extensions.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: RegistrationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.registration)
-
+        binding = RegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val name = binding.textInputName.text
+        val email = binding.textInputEmail.text
+        val enterpassword = binding.textInputEnterPassword.text
+        val confirmpassword = binding.textInputConfirmPassword.text
+        binding.textInputName.setOnClickListener {
+            binding.textInputLayoutName.error = validateName(name)
+        }
+        binding.textInputEmail.setOnClickListener {
+            binding.textInputLayoutEmail.error = validateEmail(email)
+        }
+        binding.textInputEnterPassword.setOnClickListener {
+            binding.textInputLayoutEnterPassword.error = validateEnterPassword(enterpassword)
+        }
+        binding.textInputConfirmPassword.setOnClickListener {
+            binding.textInputLayoutConfirmPassword.error = validateConfirmPassword(binding.textInputEnterPassword, binding.textInputConfirmPassword)
+        }
     }
 }
--- app/src/main/java/com/example/notes/extensions/ActivityExt.kt
+++ app/src/main/java/com/example/notes/extensions/ActivityExt.kt
@@ -1,7 +1,36 @@
 package com.example.notes.extensions
 
 import android.app.Activity
+import android.text.Editable
+import com.google.android.material.textfield.TextInputEditText
 
-fun Activity.validate(){
-
+fun Activity.validateName(text: Editable?):String?{
+    return if(text.toString().isNotEmpty())
+        null
+    else {
+        "The fields should not be empty"
+    }
+}
+fun Activity.validateEmail(text: Editable?):String? {
+    return if (text.toString().length >= 7 && text.toString().contains("@")) {
+        null
+    } else {
+        "Mail must contain at least 7 characters and \"@\""
+    }
+}
+fun Activity.validateEnterPassword(text: Editable?):String? {
+    return if (text.toString().length >= 8)  {
+        null
+    } else {
+        "Password must not be shorter than 8 characters"
+    }
+}
+fun Activity.validateConfirmPassword(password: TextInputEditText, confirmedPassword: TextInputEditText):String? {
+    val passwordValue = password.text.toString()
+    val confirmedPasswordValue = confirmedPassword.text.toString()
+    return if (passwordValue != confirmedPasswordValue) {
+        "Passwords do not match"
+    } else {
+        null
+    }
 }
